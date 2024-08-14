5d1895bd1362d1f84ef34618978211bdbd128b02
Robert Sturla <robertsturla@outlook.com>
2024-08-14 15:05:00 +0100

    fix(dx): adds a systemd unit workaround service to relabel libvirt files (#1586)

--- build_files/systemd-dx.sh
+++ build_files/systemd-dx.sh
@@ -5,6 +5,7 @@ set -ouex pipefail
 systemctl enable docker.socket
 systemctl enable podman.socket
 systemctl enable swtpm-workaround.service
+systemctl enable libvirt-workaround.service
 systemctl enable sarkinite-dx-groups.service
 systemctl enable --global sarkinite-dx-user-vscode.service
 systemctl disable pmie.service
