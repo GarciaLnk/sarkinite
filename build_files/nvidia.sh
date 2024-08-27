0862c4c136725e48c3012314e5f3cbe3a1d8762d
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2024-08-27 17:04:52 +0000

    chore: tweak nvidia settings

--- build_files/nvidia.sh
+++ build_files/nvidia.sh
@@ -8,4 +8,15 @@ if [[ ${IMAGE_FLAVOR} =~ "nvidia" || ${NVIDIA_TYPE} =~ "nvidia" ]]; then
 	if [[ ${BASE_IMAGE_NAME} =~ "kinoite" && ${FEDORA_MAJOR_VERSION} -gt "39" ]]; then
 		rpm-ostree install plasma-workspace-x11
 	fi
+
+	# Disable GSP on -nvidia builds
+	cat <<EOF >/usr/lib/modprobe.d/nvidia-gsp-workaround.conf
+options nvidia NVreg_EnableGpuScreenPreemption=0
+EOF
+
+	# Firefox Nvidia config
+	cat <<EOF >/usr/lib64/firefox/defaults/pref/00-sarkinite-nvidia.js
+pref("widget.dmabuf.force-enabled", true);
+EOF
+
 fi
