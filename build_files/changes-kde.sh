4b27dd8c188089d19bd0e3ca027b164d9917a82f
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2024-08-28 20:41:49 +0000

    fix: restore konsole entry

--- build_files/changes-kde.sh
+++ build_files/changes-kde.sh
@@ -3,8 +3,6 @@
 set -ouex pipefail
 
 if [[ ${BASE_IMAGE_NAME} == "kinoite" ]]; then
-	sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/org.kde.konsole.desktop
 	rm -f /etc/profile.d/gnome-ssh-askpass.{csh,sh} # This shouldn't be pulled in
-	rm -f /usr/share/kglobalaccel/org.kde.konsole.desktop
 	systemctl enable kde-sysmonitor-workaround.service
 fi
