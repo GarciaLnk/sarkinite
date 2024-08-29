7dc87eec6c2f2a66bf4cc2df166ed774812f0722
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2024-08-29 14:58:56 +0000

    fix: cleanup leftover repos

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -18,6 +18,8 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/ublue-os-staging-fedora-"${FED
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_che-nerd-fonts-"${FEDORA_MAJOR_VERSION}".repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-rom-properties.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-webapp-manager.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_garcia-etckeeper.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/cloudflare-warp.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 for i in /etc/yum.repos.d/rpmfusion-*; do
