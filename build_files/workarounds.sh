8b8335b38a49d052c85f59d0d7d8722a8faff760
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2024-08-29 15:57:27 +0000

    feat: add btrfs-assistant

--- build_files/workarounds.sh
+++ build_files/workarounds.sh
@@ -4,3 +4,6 @@ set -oeux pipefail
 
 # alternatives cannot create symlinks on its own during a container build
 ln -sf /usr/bin/ld.bfd /etc/alternatives/ld && ln -sf /etc/alternatives/ld /usr/bin/ld
+
+# Patch btrfs-assistant-launcher to not pass $XDG_RUNTIME_DIR so that it does not break flatpaks
+sed -i "s/ --xdg-runtime=${XDG_RUNTIME_DIR}//g" /usr/bin/btrfs-assistant-launcher
