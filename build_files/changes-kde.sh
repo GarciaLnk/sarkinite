5c7b341bc8fd5ab6f2a8b13d30d5f7f72d3836de
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2024-08-29 11:44:49 +0000

    chore: update taskbar pins

--- build_files/changes-kde.sh
+++ build_files/changes-kde.sh
@@ -3,6 +3,7 @@
 set -ouex pipefail
 
 if [[ ${BASE_IMAGE_NAME} == "kinoite" ]]; then
+	sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/filemanager,preferred:\/\/browser<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
 	rm -f /etc/profile.d/gnome-ssh-askpass.{csh,sh} # This shouldn't be pulled in
 	systemctl enable kde-sysmonitor-workaround.service
 fi
