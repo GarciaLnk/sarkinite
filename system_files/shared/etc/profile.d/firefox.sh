48f96f72616ea2238c655cf66557242e4177035c
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2024-08-30 17:23:21 +0000

    chore: add konsole profile

--- system_files/shared/etc/profile.d/firefox.sh
+++ system_files/shared/etc/profile.d/firefox.sh
@@ -3,10 +3,10 @@
 export MOZ_ENABLE_WAYLAND=1
 export MOZ_USE_XINPUT2=1
 
-if [[ $IMAGE_FLAVOR =~ "nvidia" ]] && [ "$(grep -o "\-display" <<< "$(lshw -C display)")" -le 1 ] && grep -q "vendor: NVIDIA Corporation" <<< "$(lshw -C display)"; then
-    export LIBVA_DRIVER_NAME=nvidia
-    export LIBVA_DRIVERS_PATH=/run/host/usr/lib64/dri
-    export LIBVA_MESSAGING_LEVEL=1
-    export MOZ_DISABLE_RDD_SANDBOX=1
-    export NVD_BACKEND=direct
+if [[ ${IMAGE_FLAVOR} =~ "nvidia" ]] && [[ "$(grep -o "\-display" <<<"$(lshw -C display)")" -le 1 ]] && grep -q "vendor: NVIDIA Corporation" <<<"$(lshw -C display)"; then
+	export LIBVA_DRIVER_NAME=nvidia
+	export LIBVA_DRIVERS_PATH=/run/host/usr/lib64/dri
+	export LIBVA_MESSAGING_LEVEL=1
+	export MOZ_DISABLE_RDD_SANDBOX=1
+	export NVD_BACKEND=direct
 fi
---
+++ system_files/shared/etc/skel/.local/share/konsole/Sarkinite.profile
@@ -0,0 +1,4 @@
+[General]
+Directory=~
+Name=Sarkinite
+Parent=FALLBACK/
