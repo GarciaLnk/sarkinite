116e42cb43b8b73263759a2e00a4ed5c66d7c60f
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2024-08-31 10:00:00 +0000

    chore: drop x11 on nvidia

--- build_files/nvidia.sh
+++ build_files/nvidia.sh
@@ -4,11 +4,6 @@ set -ouex pipefail
 
 # Nvidia Configurations
 if [[ ${IMAGE_FLAVOR} =~ "nvidia" || ${NVIDIA_TYPE} =~ "nvidia" ]]; then
-	# Restore x11 for Nvidia Images
-	if [[ ${BASE_IMAGE_NAME} =~ "kinoite" && ${FEDORA_MAJOR_VERSION} -gt "39" ]]; then
-		rpm-ostree install plasma-workspace-x11
-	fi
-
 	# Disable GSP on -nvidia builds
 	cat <<EOF >/usr/lib/modprobe.d/nvidia-gsp-workaround.conf
 options nvidia NVreg_EnableGpuScreenPreemption=0
