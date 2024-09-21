ff6247befd0d0853dbdc4975f6a81d54f3b501ab
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2024-09-21 14:28:12 +0000

    feat: add custom arkenfox user.js and other firefox prefs

--- system_files/shared/usr/lib64/firefox/defaults/pref/01-sarkinite-global.js
+++ system_files/shared/usr/lib64/firefox/defaults/pref/01-sarkinite-global.js
@@ -1,2 +1,9 @@
+// Hardware acceleration
 pref("gfx.webrender.all", true);
 pref("media.ffmpeg.vaapi.enabled", true);
+
+// Unpin pages from new tab
+pref("browser.newtabpage.pinned", "");
+
+// Unload tabs on low memory
+pref("browser.tabs.unloadOnLowMemory", true);
