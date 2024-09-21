35d693c9fc387534bc44245d51e725f143cbc851
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2024-09-21 18:06:42 +0000

    fix: remove unneeded env variables to enable firefox hwdec on nvidia

--- system_files/shared/etc/profile.d/firefox.sh
+++ system_files/shared/etc/profile.d/firefox.sh
@@ -4,9 +4,5 @@ export MOZ_ENABLE_WAYLAND=1
 export MOZ_USE_XINPUT2=1
 
 if [[ $(cat /etc/ublue/image_flavor) =~ "nvidia" ]] && [[ "$(grep -o "\-display" <<<"$(lshw -C display)")" -le 1 ]] && grep -q "vendor: NVIDIA Corporation" <<<"$(lshw -C display)"; then
-	export LIBVA_DRIVER_NAME=nvidia
-	export LIBVA_DRIVERS_PATH=/run/host/usr/lib64/dri
-	export LIBVA_MESSAGING_LEVEL=1
 	export MOZ_DISABLE_RDD_SANDBOX=1
-	export NVD_BACKEND=direct
 fi
