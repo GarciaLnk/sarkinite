9b231f5658207a9fa3f91ddd36df0b6585453613
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-09-28 03:57:09 -0700

    feat: Add os-release changes from Bazzite (#1708)

--- build_files/image-info.sh
+++ build_files/image-info.sh
@@ -2,6 +2,9 @@
 
 set -ouex pipefail
 
+IMAGE_PRETTY_NAME="Sarkinite"
+IMAGE_LIKE="fedora"
+
 IMAGE_INFO="/usr/share/ublue-os/image-info.json"
 IMAGE_REF="ostree-image-signed:docker://ghcr.io/${IMAGE_VENDOR}/${IMAGE_NAME}"
 
@@ -24,4 +27,18 @@ cat >"${IMAGE_INFO}" <<EOF
 }
 EOF
 
-sed -i "s/VARIANT_ID.*/VARIANT_ID=${IMAGE_NAME}/" /etc/os-release
+# OS Release File
+sed -i "s/^VARIANT_ID=.*/VARIANT_ID=${IMAGE_NAME}/" /usr/lib/os-release
+sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME=\"Bluefin ${FEDORA_MAJOR_VERSION} (FROM Fedora ${BASE_IMAGE_NAME^})\"/" /usr/lib/os-release
+sed -i "s/^NAME=.*/NAME=\"${IMAGE_PRETTY_NAME}\"/" /usr/lib/os-release
+sed -i "s|^CPE_NAME=\"cpe:/o:fedoraproject:fedora|CPE_NAME=\"cpe:/o:universal-blue:${IMAGE_PRETTY_NAME,}|" /usr/lib/os-release
+sed -i "s/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME=\"${IMAGE_PRETTY_NAME,}\"/" /usr/lib/os-release
+sed -i "s/^ID=fedora/ID=${IMAGE_PRETTY_NAME,}\nID_LIKE=\"${IMAGE_LIKE}\"/" /usr/lib/os-release
+sed -i "/^REDHAT_BUGZILLA_PRODUCT=/d; /^REDHAT_BUGZILLA_PRODUCT_VERSION=/d; /^REDHAT_SUPPORT_PRODUCT=/d; /^REDHAT_SUPPORT_PRODUCT_VERSION=/d" /usr/lib/os-release
+
+if [[ -n ${SHA_HEAD_SHORT-} ]]; then
+	echo "BUILD_ID=\"${SHA_HEAD_SHORT}\"" >>/usr/lib/os-release
+fi
+
+# Fix issues caused by ID no longer being fedora
+sed -i 's/^EFIDIR=.*/EFIDIR="fedora"/' /usr/sbin/grub2-switch-to-blscfg
