d21a0d6e0287cbed37e4496f3d16a3bb5b706f24
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2024-10-30 08:30:25 +0000

    chore: update elfutils

--- build_files/update.sh
+++ build_files/update.sh
@@ -2,7 +2,24 @@
 
 set -ouex pipefail
 
+# This script provides fixes to packages known to have caused build skew.
+# It works by force replacing packages on the FROM image with current
+# packages from fedora update repos.
+
+repos=(
+	fedora-updates.repo
+	fedora-updates-archive.repo
+)
+
+for repo in "${repos[@]}"; do
+	if [[ $(grep -c "enabled=1" /etc/yum.repos.d/"${repo}") -eq 0 ]]; then
+		sed -i "0,/enabled=0/{s/enabled=0/enabled=1/}" /etc/yum.repos.d/"${repo}"
+	fi
+done
+
 # Update packages that commonly cause build issues
+rpm-ostree override replace --experimental --from repo=updates \
+	elfutils-libelf elfutils-libs || true
 rpm-ostree override replace --experimental --from repo=updates \
 	vulkan-loader || true
 rpm-ostree override replace --experimental --from repo=updates \
