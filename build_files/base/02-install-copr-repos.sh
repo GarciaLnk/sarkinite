1d4dcbf013021ab8ae71a85fd1d0e53545a440bb
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2024-12-14 18:14:42 +0100

    chore: add terra repo and remove redundant repos

--- build_files/base/02-install-copr-repos.sh
+++ build_files/base/02-install-copr-repos.sh
@@ -10,25 +10,14 @@ curl --retry 3 -Lo /etc/yum.repos.d/ublue-os-staging-fedora-"$(rpm -E %fedora)".
 curl --retry 3 -Lo /etc/yum.repos.d/_copr_sentry-switcheroo-control_discrete.repo \
 	https://copr.fedorainfracloud.org/coprs/sentry/switcheroo-control_discrete/repo/fedora-"$(rpm -E %fedora)"/sentry-switcheroo-control_discrete-fedora-"$(rpm -E %fedora)".repo
 
-# Add Nerd Fonts Repo
-curl --retry 3 -Lo /etc/yum.repos.d/_copr_che-nerd-fonts-"$(rpm -E %fedora)".repo https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"$(rpm -E %fedora)"/che-nerd-fonts-fedora-"$(rpm -E %fedora)".repo
-
 # Add ROM Properties
 curl --retry 3 -Lo /etc/yum.repos.d/_copr_kylegospo-rom-properties.repo https://copr.fedorainfracloud.org/coprs/kylegospo/rom-properties/repo/fedora-"${FEDORA_MAJOR_VERSION}"/kylegospo-rom-properties-fedora-"${FEDORA_MAJOR_VERSION}".repo
 
 # Add Webapp Manager
 curl --retry 3 -Lo /etc/yum.repos.d/_copr_kylegospo-webapp-manager.repo https://copr.fedorainfracloud.org/coprs/kylegospo/webapp-manager/repo/fedora-"${FEDORA_MAJOR_VERSION}"/kylegospo-webapp-manager-fedora-"${FEDORA_MAJOR_VERSION}".repo
 
-# Add etckeeper
-if [[ ${FEDORA_MAJOR_VERSION} -lt "41" ]]; then
-	curl --retry 3 -Lo /etc/yum.repos.d/_copr_garcia-etckeeper.repo https://copr.fedorainfracloud.org/coprs/garcia/etckeeper/repo/fedora-"${FEDORA_MAJOR_VERSION}"/garcia-etckeeper-fedora-"${FEDORA_MAJOR_VERSION}".repo
-fi
-
-# Add CoolerControl
-curl --retry 3 -Lo /etc/yum.repos.d/_copr_codifryed-CoolerControl.repo https://copr.fedorainfracloud.org/coprs/codifryed/CoolerControl/repo/fedora-"${FEDORA_MAJOR_VERSION}"/codifryed-CoolerControl-fedora-"${FEDORA_MAJOR_VERSION}".repo
-
 # Add FirefoxPWA
 echo -e "[firefoxpwa]\nname=FirefoxPWA\nmetadata_expire=300\nbaseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch\ngpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey\nrepo_gpgcheck=1\ngpgcheck=0\nenabled=1" | tee /etc/yum.repos.d/firefoxpwa.repo
 
-# Add keyd
-curl --retry 3 -Lo /etc/yum.repos.d/_copr_alternateved-keyd.repo https://copr.fedorainfracloud.org/coprs/alternateved/keyd/repo/fedora-"${FEDORA_MAJOR_VERSION}"/alternateved-keyd-fedora-"${FEDORA_MAJOR_VERSION}".repo
+# Add Terra repo
+curl --retry 3 -Lo /etc/yum.repos.d/terra.repo https://raw.githubusercontent.com/terrapkg/packages/refs/heads/frawhide/anda/terra/release/terra.repo
--- build_files/base/17-cleanup.sh
+++ build_files/base/17-cleanup.sh
@@ -41,15 +41,10 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.r
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/tailscale.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/charm.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_che-nerd-fonts-"${FEDORA_MAJOR_VERSION}".repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-rom-properties.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-webapp-manager.repo
-if [[ ${FEDORA_MAJOR_VERSION} -lt "41" ]]; then
-	sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_garcia-etckeeper.repo
-fi
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_codifryed-CoolerControl.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/firefoxpwa.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_alternateved-keyd.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/terra.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 for i in /etc/yum.repos.d/rpmfusion-*; do
