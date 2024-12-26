d06d60f728f20c493e0fb9bd7fe1e3030307b860
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2024-12-26 20:58:55 +0100

    fix: fcitx env setup

--- system_files/kinoite/etc/profile.d/fcitx5-kde.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-[ "${GTK_IM_MODULE}" = "fcitx" ] && unset GTK_IM_MODULE
-[ "${QT_IM_MODULE}" = "fcitx" ] && unset QT_IM_MODULE
---
+++ system_files/kinoite/etc/profile.d/fcitx5.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ ! "${XDG_SESSION_TYPE}" = "tty" ]   # if this is a gui session (not tty)
+then
+    # let's use fcitx instead of fcitx5 to make flatpak happy
+    # this may break behavior for users who have installed both
+    # fcitx and fcitx5, let then change the file on their own
+    export INPUT_METHOD=fcitx
+    export XMODIFIERS=@im=fcitx
+fi
