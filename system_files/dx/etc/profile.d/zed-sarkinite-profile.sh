0320a0a80aa0df6abaf1f17a2e07b4b7bf8c92e6
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2025-01-16 23:13:52 +0100

    fix: cp zed and ghostty settings

---
+++ system_files/dx/etc/profile.d/zed-sarkinite-profile.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+if test "$(id -u)" -gt "0" && test -d "${HOME}"; then
+	# Add default settings when there are no settings
+	if test ! -e "${HOME}"/.config/zed/settings.json; then
+		mkdir -p "${HOME}"/.config/zed
+		cp -f /etc/skel/.config/zed/settings.json "${HOME}"/.config/zed/settings.json
+	fi
+fi
---
+++ system_files/kinoite/etc/profile.d/ghostty-sarkinite-profile.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+if test "$(id -u)" -gt "0" && test -d "${HOME}"; then
+	# Add default settings when there are no settings
+	if test ! -e "${HOME}"/.config/ghostty/config; then
+		mkdir -p "${HOME}"/.config/ghostty
+		cp -f /etc/skel/.config/ghostty/config "${HOME}"/.config/ghostty/config
+	fi
+fi
