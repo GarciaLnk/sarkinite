f24cf13009e148128ed70676864c6edc3f8f4d92
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2025-02-02 23:35:01 +0100

    fix: copy flatpak list on image to compare against it on boot

--- build_files/base/18-workarounds.sh
+++ build_files/base/18-workarounds.sh
@@ -13,4 +13,16 @@ fi
 # Patch btrfs-assistant-launcher to not pass $XDG_RUNTIME_DIR so that it does not break flatpaks
 sed -i 's/ --xdg-runtime=\\"${XDG_RUNTIME_DIR}\\"//g' /usr/bin/btrfs-assistant-launcher
 
+# Copy flatpak list on image to compare against it on boot wo/ requiring curl to gh
+if [[ ${IMAGE_NAME} =~ "gnome" ]]; then
+	FLATPAKS="flatpaks_gnome/flatpaks"
+elif [[ ${IMAGE_NAME} =~ "kde" ]]; then
+	FLATPAKS="flatpaks_kde/flatpaks"
+fi
+FLATPAK_LIST=($(curl https://raw.githubusercontent.com/GarciaLnk/sarkinite/main/${FLATPAKS}))
+if [[ ${IMAGE_NAME} =~ "dx" ]]; then
+	FLATPAK_LIST+=($(curl https://raw.githubusercontent.com/GarciaLnk/sarkinite/main/dx_flatpaks/flatpaks))
+fi
+printf "%s\n" "${FLATPAK_LIST[@]}" > /usr/share/ublue-os/flatpak-list
+
 echo "::endgroup::"
