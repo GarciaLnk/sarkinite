ublue-os/aurora

chore: un-cliwrap and use dnf5 instead of rpm-ostree (#174)
* chore: un-cliwrap and use dnf5 instead of rpm-ostree

I've been tinkering with and testing dnf5 for a while.

Key items are:
- ensuring cliwrap is properly unwound (checked rpm-ostree Rust code)
- using normal (not wrapped) binaries, eg for dracut
- and the more obvious, replacing "rpm-ostree install" with "dnf5
  install", etc

Relates: #1946

* remove build-fix since not needed with dnf5

* convert a few more commands from rpm-ostree to dnf5

the nvidia script from hwe repo had been missed before
the logo swap is new

* Update 03-install-kernel-akmods.sh

* Update 03-install-kernel-akmods.sh

* Update 03-install-kernel-akmods.sh

* Update 02-install-kernel-akmods-dx.sh

* Update 09-hwe-additions.sh

* Appease the linter

* fix: Install kernel-devel

* Update 05-override-install.sh

* Update 07-base-image-changes.sh

* Update 03-install-kernel-akmods.sh

* Delete build_files/base/01-build-fix.sh

* Update 05-override-install.sh

* Update 05-override-install.sh

---------

Co-authored-by: Benjamin Sherman <benjamin@holyarmy.org>
Co-authored-by: m2 <69128853+m2Giles@users.noreply.github.com>
Co-authored-by: Niklas <40402114+NiHaiden@users.noreply.github.com>

build_files/base/17-cleanup.sh
@@ -31,8 +31,10 @@ sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applica
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/tailscale.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/charm.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_che-nerd-fonts-"${FEDORA_MAJOR_VERSION}".repo
+dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable che/nerd-fonts
+dnf5 -y copr disable phracek/PyCharm
+# NOTE: we won't use dnf5 copr plugin for ublue-os/akmods until our upstream provides the COPR standard naming
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/hardware:razer.repo

GarciaLnk/sarkinite

build_files/17-cleanup.sh
@@ -51,17 +51,19 @@ sed -i 's@Exec=waydroid first-launch@Exec=/usr/bin/waydroid-launcher first-launc
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/tailscale.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/charm.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-staging.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-packages.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-rom-properties.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-webapp-manager.repo
+dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable ublue-os/packages
+dnf5 -y copr disable kylegospo/rom-properties
+dnf5 -y copr disable kylegospo/webapp-manager
+dnf5 -y copr disable lizardbyte/stable
+dnf5 -y copr disable hikariknight/looking-glass-kvmfr
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/firefoxpwa.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/terra.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_lizardbyte-stable.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_hikariknight-looking-glass-kvmfr.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/vscode.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/docker-ce.repo
+# NOTE: we won't use dnf5 copr plugin for ublue-os/akmods until our upstream provides the COPR standard naming
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 
 for i in /etc/yum.repos.d/rpmfusion-*; do
 	sed -i 's@enabled=1@enabled=0@g' "${i}"