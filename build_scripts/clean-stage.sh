4a3e31bd8c07d300363d6d95ed12f0bfcde4e763
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2025-03-05 20:33:29 +0100

    feat: enable composefs

--- build_scripts/clean-stage.sh
+++ build_scripts/clean-stage.sh
@@ -5,6 +5,7 @@ echo "::group:: ===$(basename "$0")==="
 set -eoux pipefail
 
 # dnf cleanup
+dnf5 remove -y ostree-grub2
 dnf5 autoremove -y
 dnf5 clean all
 
@@ -19,5 +20,7 @@ mkdir -p /boot
 
 find /var/log -type f -exec bash -c '[ -s "$1" ] && rm "$1"' _ {} \;
 find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
+
+bootc container lint --fatal-warnings
 
 echo "::endgroup::"
