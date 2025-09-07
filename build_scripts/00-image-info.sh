e49885a94749336f3d16ad970e7f97eb0c341170
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2025-09-07 12:08:35 +0200

    fix(os-release): remove extra image info from os-release

--- build_scripts/00-image-info.sh
+++ build_scripts/00-image-info.sh
@@ -29,7 +29,7 @@ sed -i "s|^NAME=.*|NAME=Sarkinite|" /usr/lib/os-release
 sed -i "s|^VERSION=.*|VERSION=\"${VERSION} (Kinoite)\"|" /usr/lib/os-release
 sed -i "s|^ID=fedora|ID=sarkinite\nID_LIKE=fedora|" /usr/lib/os-release
 sed -i "s|^VARIANT_ID=.*|VARIANT_ID=${IMAGE_NAME}|" /usr/lib/os-release
-sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"Sarkinite (Version: ${VERSION} / FROM Fedora Kinoite ${FEDORA_MAJOR_VERSION})\"|" /usr/lib/os-release
+sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"Sarkinite (Version: ${VERSION})\"|" /usr/lib/os-release
 sed -i "s|^CPE_NAME=\"cpe:/o:fedoraproject:fedora|CPE_NAME=\"cpe:/o:garcialnk:sarkinite:${VERSION}|" /usr/lib/os-release
 sed -i "s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME=sarkinite|" /usr/lib/os-release
 sed -i "/^REDHAT_BUGZILLA_PRODUCT=/d; /^REDHAT_BUGZILLA_PRODUCT_VERSION=/d; /^REDHAT_SUPPORT_PRODUCT=/d; /^REDHAT_SUPPORT_PRODUCT_VERSION=/d" /usr/lib/os-release
