a0e0ad36da7f97c7749340099f79527bbb219b8d
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2025-11-18 20:21:53 +0100

    chore: cleanup sarkinite-firstboot leftover

--- system_files/etc/profile.d/sarkinite-firstboot.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if test "$(id -u)" -gt "0" && test -d "${HOME}"; then
-	if test ! -e "${HOME}"/.config/autostart/sarkinite-firstboot.desktop; then
-		mkdir -p "${HOME}"/.config/autostart
-		cp -f /etc/skel/.config/autostart/sarkinite-firstboot.desktop "${HOME}"/.config/autostart
-	fi
-fi
